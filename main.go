5aa2c3a7be9f14ec2747c02c9553d2176335e1d8
Brian Antunes <brian.antunes@yahoo.com>
2023-09-19 15:40:06 -0400

    JWT madness with chi

--- main.go
+++ main.go
@@ -4,16 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-var router *chi.Mux
 var db *sql.DB
+var tokenAuth *jwtauth.JWTAuth
+
+const Secret = "jwt-secret"
 
 type Article struct {
 	ID      int           `json:"id"`
@@ -28,48 +34,95 @@ func catch(err error) {
 	}
 }
 
+func MakeToken(name string) string {
+	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"username": name})
+	return tokenString
+}
+
 func init() {
 	load := godotenv.Load()
 	if load != nil {
 		fmt.Print("Error loading .env file")
 	}
 
-	router = chi.NewRouter()
-	router.Use(middleware.Recoverer)
+	tokenAuth = jwtauth.New("HS256", []byte(Secret), nil)
 
 	var err error
 	db, err = connect()
 	catch(err)
 }
 
-func main() {
-	router = chi.NewRouter()
-	router.Use(middleware.Recoverer)
+func router() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	var err error
-	db, err = connect()
-	catch(err)
+	r.Use(ChangeMethod)
 
-	router.Use(ChangeMethod)
-	router.Get("/", GetAllArticles)
-	router.Post("/upload", UploadHandler) // Add this
-	router.Get("/images/*", ServeImages)  // Add this
-	router.Route("/articles", func(r chi.Router) {
-		r.Get("/", NewArticle)
-		r.Post("/", CreateArticle)
-		r.Route("/{articleID}", func(r chi.Router) {
-			r.Use(ArticleCtx)
-			r.Get("/", GetArticle)       // GET /articles/1234
-			r.Put("/", UpdateArticle)    // PUT /articles/1234
-			r.Delete("/", DeleteArticle) // DELETE /articles/1234
-			r.Get("/edit", EditArticle)  // GET /articles/1234/edit
+	// Protected Routes
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+
+		r.Post("/upload", UploadHandler)
+		r.Route("/articles", func(r chi.Router) {
+			r.Get("/", NewArticle)
+			r.Post("/", CreateArticle)
+			r.Route("/{articleID}", func(r chi.Router) {
+				r.Use(ArticleCtx)
+				r.Put("/", UpdateArticle)    // PUT /articles/1234
+				r.Delete("/", DeleteArticle) // DELETE /articles/1234
+				r.Get("/edit", EditArticle)  // GET /articles/1234/edit
+			})
+		})
+		r.Post("/signin", func(w http.ResponseWriter, r *http.Request) {
+			r.ParseForm()
+			userName := r.PostForm.Get("username")
+			userPassword := r.PostForm.Get("password")
+
+			if userName == "" || userPassword == "" {
+				http.Error(w, "missing user or password", http.StatusBadRequest)
+				return
+			}
+
+			token := MakeToken(userName)
+
+			http.SetCookie(w, &http.Cookie{
+				HttpOnly: true,
+				Expires:  time.Now().Add(7 * 24 * time.Hour),
+				SameSite: http.SameSiteLaxMode,
+				// Uncomment below for HTTPS:
+				// Secure: true,
+				Name:  "jwt", // Must be named "jwt" or else the token cannot be searched for by jwtauth.Verifier.
+				Value: token,
+			})
 		})
 	})
-	router.Post("/signin", Signin)
-	router.Get("/welcome", Welcome)
-	router.Post("/refresh", Refresh)
-	router.Post("/logout", Logout)
 
-	err = http.ListenAndServe(":8005", router)
-	catch(err)
+	// Public Routes
+	r.Get("/", GetAllArticles)
+	r.Get("/images/*", ServeImages) // Add this
+	r.Route("/articles", func(r chi.Router) {
+		r.Route("/{articleID}", func(r chi.Router) {
+			r.Use(ArticleCtx)
+			r.Get("/", GetArticle) // GET /articles/1234
+		})
+	})
+
+	// r.Post("/signin", Signin)
+	// r.Get("/welcome", Welcome)
+	// r.Post("/refresh", Refresh)
+	// r.Post("/logout", Logout)
+
+	return r
+}
+
+func main() {
+	port := "8080"
+
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, router()))
 }
